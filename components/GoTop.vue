607bedc72d2ead54392298c525996958be76ddf0
Mathieu <72748304+mathieubes@users.noreply.github.com>
2021-04-08 22:37:32 +0200

    GoTopButton

---
+++ components/GoTop.vue
@@ -0,0 +1,17 @@
+<template>
+    <a class="go-top" href="#">
+        Go top <img :src="goTopIcon" />
+    </a>
+</template>
+
+<script>
+import GoTopIcon from '@/assets/images/GoTop.svg';
+
+export default {
+    data() {
+        return {
+            goTopIcon: GoTopIcon
+        }
+    }
+}
+</script>
