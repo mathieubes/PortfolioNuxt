a6281cfac62a210ab8e4d2fa7bd725dcea999e0e
Mathieu <72748304+mathieubes@users.noreply.github.com>
2021-04-17 10:43:24 +0200

    About page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,23 @@
 <template>
     <div class="container about-container">
+        <div class="flex">
+            <div class="picture">
+                <img src="~/assets/images/me.png" alt="Picture of me">
+            </div>
+            <div class="content">
+                <h1 class="title uppercased">Joy of living</h1>
+                <p>
+                    I am often defined as the joy of living.<br/>
+                    <br/>
+                    And I try to show it as much as possible in the projects I am involved in.<br/>
+                    <br/>
+                    French developer I don't just have to write lines of code, I am a source of ideas and innovations.
+                </p>
+            </div>
+        </div>
 
+        <div class="emojis">
+            🇫🇷 😁 💻
+        </div>
     </div>
 </template>
